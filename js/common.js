7cfd7ad16e9aaeb3fc9f04d28b941ff766ce5f58
rihno1 <redwoodweb@naver.com>
2020-11-18 16:35:10 +0900

    hash scroll animate update
    hash scroll animate update

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(function(){
   //script 입력영역
+  $("nav ul li a,.back_to_top a,.footer_top a,a.button").click(function(e){
+    // console.log($(this.hash).offset().top)
+    var thisElem = $(this.hash);
+    $("html,body").stop();
+    $("html,body").animate({scrollTop : thisElem.offset().top},1500);
+    //e.preventDefault();
+    return false;
+  });
+
 });
